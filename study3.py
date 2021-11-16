9874584b4c5d5b0fd785c040c47b8c3faf621c64
Jacob <jas111213@gmail.com>
2021-11-16 14:49:53 -0500

    reverse tuple

--- study3.py
+++ study3.py
@@ -6,9 +6,14 @@ def make_tuple(a, b, c):
     tup = (a, b, c)
     return tup
 
-def main():
-    pass
+def reverse_tuple(sequence):
+    new_list = sequence[::-1]
+    new_tup = tuple(new_list)
+    return new_tup
 
+def main():
+    print(reverse_tuple([1, 3, 4, 5, 6, 7]))
+    print(reverse_tuple((1, 5, 6, 6, 7)))
 
 if __name__ == "__main__":
     main()
--- study3_test.py
+++ study3_test.py
@@ -7,3 +7,10 @@ from study3 import *
 def test_make_tuple():
     tuple = make_tuple("a", 1, 2)
     assert ("a", 1, 2) == tuple
+
+def test_reverse_tuple():
+    a_list = ["a", 1, "b"]
+    a_tuple = (1, "b", 3)
+
+    assert ("b", 1, "a") == reverse_tuple(a_list)
+    assert (3, "b", 1) == reverse_tuple(a_tuple)
